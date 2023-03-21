7322d83665ee0917feebdb025a828a7486faba3e
mplug <oppoalien@gmail.com>
2023-03-22 03:18:26 +0700

    feat: do the custom calendar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import carousalPic1 from "../assets/images/Co-Working-Space1.jpeg";
 import carousalPic2 from "../assets/images/Co-Working-Space2.jpeg";
 import carousalPic3 from "../assets/images/Co-Working-Space3.jpeg";
 import carousalPic4 from "../assets/images/Co-Working-Space4.jpeg";
 import newsPic1 from "../assets/images/Co-Working-Space5.png";
 import { Link } from "react-router-dom";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
 
 export const Home = () => {
+  const [currCrsIndex, setCurrCrsIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrCrsIndex((currCrsIndex) => {
+        return currCrsIndex === slides.length - 1 ? 0 : currCrsIndex + 1;
+      });
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const slides = [
+    {
+      url: "https://images.unsplash.com/photo-1571624436279-b272aff752b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1557367184-663fba4b8b91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1497366754035-f200968a6e72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+    },
+  ];
+
+  const prevCrsBtn = () => {
+    setCurrCrsIndex(currCrsIndex === 0 ? slides.length - 1 : currCrsIndex - 1);
+  };
+
+  const nextCrsBtn = () => {
+    setCurrCrsIndex(currCrsIndex === slides.length - 1 ? 0 : currCrsIndex + 1);
+  };
+
   return (
     <div className="w-full h-full flex justify-center text-font-primary font-prompt text-sm md:mx-auto p-4 py-20">
-      <div className="w-full md:w-3/4 flex flex-col gap-y-8">
+      <div className="w-full lg:w-3/4 flex flex-col gap-y-8">
         <div className="recommendBlock flex flex-col gap-y-2 md:gap-y-4">
           <div className="w-full md:w-fit bg-orange-300 rounded-lg p-2 px-4">
             <h1 className="font-medium">Co-Working แนะนำ</h1>
           </div>
-          <Link to="/branch/1" className="carousalBlock w-full hover:shadow-lg">
-            <img
-              src={carousalPic2}
-              alt=""
-              className="object-cover object-center max-h-[400px] w-full rounded-md"
-            />
-          </Link>
+          <div className="w-full h-[400px] relative group mb-8">
+            <div
+              style={{ backgroundImage: `url(${slides[currCrsIndex]?.url})` }}
+              className="w-full h-full rounded-lg bg-center bg-cover duration-500"
+            >
+              {/* left arrow */}
+              <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                <BsChevronCompactLeft onClick={prevCrsBtn} size={30} />
+              </div>
+              {/* right arrow */}
+              <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                <BsChevronCompactRight onClick={nextCrsBtn} size={30} />
+              </div>
+            </div>
+            <div className="flex top-4 justify-center py-2">
+              {slides.map((slide, idx) => (
+                <div
+                  key={`dot_${idx}`}
+                  onClick={() => setCurrCrsIndex(idx)}
+                  className="text-2xl cursor-pointer"
+                >
+                  <RxDotFilled />
+                </div>
+              ))}
+            </div>
+          </div>
           <div className="listBlock w-full grid grid-cols-4 gap-2 md:gap-4">
             <Link
               to="/branch/1"
--- src/pages/external/Reserve.jsx
+++ src/pages/external/Reserve.jsx
@@ -1,88 +1,288 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 export const Reserve = () => {
+  //day of week and month start index 0
+  const [yearData, setYearData] = useState(0);
+  const [monthData, setMonthData] = useState(0);
+  const [dateData, setDateData] = useState(0);
+  const [amountDateThisMonth, setAmountDateThisMonth] = useState(0);
+  const [amountDateLastMonth, setAmountDateLastMonth] = useState(0);
+  const [dayMonthStart, setDayMonthStart] = useState(0);
+  const [calendar, setCalendar] = useState([]);
+  const [selectDateTime, setSelectDateTime] = useState({
+    year: 0,
+    month: 0,
+    date: 0,
+    day: 0,
+    key: 0,
+  });
+  const [tabSelect, setTabSelect] = useState("room");
+
+  const dayString = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  const monthString = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  useEffect(() => {
+    setYearData(new Date().getFullYear());
+    setMonthData(new Date().getMonth() + 1);
+    setDateData(new Date().getDate());
+  }, []);
+
+  useEffect(() => {
+    if (monthData !== 0 && yearData !== 0) {
+      setAmountDateThisMonth(new Date(yearData, monthData, 0).getDate());
+      setAmountDateLastMonth(
+        new Date(
+          monthData === 1 ? yearData - 1 : yearData,
+          monthData === 1 ? 12 : monthData - 1,
+          0
+        ).getDate()
+      );
+      setDayMonthStart(new Date(`${yearData}-${monthData}-01`).getDay() + 1);
+    }
+  }, [monthData, yearData]);
+
+  useEffect(() => {
+    if (amountDateLastMonth !== 0 && amountDateThisMonth !== 0) {
+      const calendar = Object.keys([
+        ...Array(dayMonthStart > 5 ? 6 * 7 : 5 * 7),
+      ]).map((key) => {
+        const newKey = Number(key);
+        let dateCalendar = 0;
+        let dayCalendar = 0;
+        let monthCalendar = 0;
+        let yearCalendar = 0;
+
+        const commute = amountDateLastMonth - dayMonthStart + newKey + 1 + 1;
+
+        //next month
+        if (commute > amountDateLastMonth) {
+          if (commute > amountDateLastMonth + amountDateThisMonth) {
+            dateCalendar =
+              commute - (amountDateLastMonth + amountDateThisMonth);
+            dayCalendar =
+              new Date(
+                `${monthData === 12 ? yearData + 1 : yearData}-${
+                  monthData === 12 ? 1 : monthData + 1
+                }-${dateCalendar}`
+              ).getDay() + 1;
+            monthCalendar = monthData + 1;
+            yearCalendar = monthData === 12 ? yearData + 1 : yearData;
+
+            //curr month
+          } else {
+            dateCalendar = commute - amountDateLastMonth;
+            dayCalendar =
+              new Date(`${yearData}-${monthData}-${dateCalendar}`).getDay() + 1;
+            monthCalendar = monthData;
+            yearCalendar = yearData;
+          }
+
+          //prev month
+        } else {
+          dateCalendar = commute;
+          dayCalendar =
+            new Date(
+              `${monthData === 1 ? yearData - 1 : yearData}-${
+                monthData === 1 ? 12 : monthData - 1
+              }-${dateCalendar}`
+            ).getDay() + 1;
+          monthCalendar = monthData - 1;
+          yearCalendar = monthData === 1 ? yearData - 1 : yearData;
+        }
+
+        return {
+          key: newKey,
+          day: dayCalendar,
+          date: dateCalendar,
+          month: monthCalendar,
+          year: yearCalendar,
+        };
+      });
+      // console.log(calendar, calendar);
+      setCalendar(calendar);
+    }
+  }, [amountDateLastMonth, amountDateThisMonth]);
+
+  // console.log(
+  //   yearData,
+  //   monthData,
+  //   dateData,
+  //   amountDateLastMonth,
+  //   amountDateThisMonth,
+  //   dayMonthStart
+  // );
+
   return (
-    <div className="relative w-full md:h-screen flex justify-center text-font-primary font-prompt text-sm md:mx-auto p-4 pt-20 md:pt-4">
-      <div className="w-full md:w-3/4 flex justify-center items-center gap-y-8">
+    <div className="relative w-full flex justify-center text-font-primary font-prompt text-sm md:mx-auto p-4 pt-20">
+      <div className="w-full lg:w-3/4 flex justify-center items-center gap-y-8">
         <div className="mainBlock w-full flex flex-col md:flex-row gap-4">
           <div className="calendarBlock w-full md:w-7/12 flex flex-col gap-4">
             <div>
               <h1 className="text-2xl">Too fast too sleep สาขา 1 </h1>
             </div>
             <div className="cBlock">
-              <div className="w-full bg-orange-100 rounded-[30px] grid grid-cols-7 gap-x-1 gap-y-8 p-2 md:p-4">
-                {[...Array(31)].map((r, idx) => (
+              <div className="w-full flex flex-col gap-y-4 bg-orange-100 rounded-[15px] p-2 md:p-4">
+                <div className="yearBlock flex justify-between items-center">
                   <div
-                    key={`date_${idx}`}
-                    className="w-full flex justify-center items-center"
+                    onClick={() => setYearData(yearData - 1)}
+                    className="w-fit bg-orange-300 hover:bg-orange-400 text-white rounded-full cursor-pointer p-2"
                   >
-                    <Link>
-                      <div className="w-12 h-12 flex justify-center items-center bg-gray-300 hover:bg-gray-400 rounded-full p-4">
-                        {idx + 1}
-                      </div>
-                    </Link>
+                    <FaAngleLeft />
                   </div>
-                ))}
+                  <p className="font-medium">{yearData}</p>
+                  <div
+                    onClick={() => setYearData(yearData + 1)}
+                    className="w-fit bg-orange-300 hover:bg-orange-400 text-white rounded-full cursor-pointer p-2"
+                  >
+                    <FaAngleRight />
+                  </div>
+                </div>
+                <div className="monthBlock flex justify-between items-center">
+                  <div
+                    onClick={() =>
+                      setMonthData(monthData === 1 ? 12 : monthData - 1)
+                    }
+                    className="w-fit bg-orange-300 hover:bg-orange-400 text-white rounded-full cursor-pointer p-2"
+                  >
+                    <FaAngleLeft />
+                  </div>
+                  <p className="font-medium">{monthString[monthData - 1]}</p>
+                  <div
+                    onClick={() =>
+                      setMonthData(monthData === 12 ? 1 : monthData + 1)
+                    }
+                    className="w-fit bg-orange-300 hover:bg-orange-400 text-white rounded-full cursor-pointer p-2"
+                  >
+                    <FaAngleRight />
+                  </div>
+                </div>
+                <div className="dayBlock grid grid-cols-7 gap-x-1">
+                  {dayString.map((day, idx) => (
+                    <p
+                      key={`day_${idx}`}
+                      className="text-[0.75rem] text-center"
+                    >
+                      {day}
+                    </p>
+                  ))}
+                </div>
+                <div className="grid grid-cols-7 gap-x-1 gap-y-6">
+                  {calendar.map((r, idx) => (
+                    <div
+                      key={`key_${idx}`}
+                      className="w-full flex justify-center items-center"
+                    >
+                      {r.month === monthData && (
+                        <button
+                          onClick={() =>
+                            setSelectDateTime({
+                              year: r.year,
+                              month: r.month,
+                              date: r.date,
+                              day: r.day,
+                              key: r.key,
+                            })
+                          }
+                        >
+                          <div
+                            className={`w-12 h-12 flex justify-center items-center ${
+                              selectDateTime.date === r.date &&
+                              selectDateTime.month === r.month &&
+                              selectDateTime.year === r.year
+                                ? "bg-orange-400/50 border-2 border-orange-500"
+                                : new Date().getDate() === r.date &&
+                                  new Date().getMonth() + 1 === r.month &&
+                                  new Date().getFullYear() === r.year
+                                ? "bg-orange-400/40"
+                                : "bg-orange-300/30"
+                            } hover:bg-orange-400/50 rounded-full p-4`}
+                          >
+                            {r.date}
+                          </div>
+                        </button>
+                      )}
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
 
           <div className="detailBlock w-full md:w-5/12 flex flex-col gap-2">
             <div className="flex gap-x-2">
-              <button className="w-fit bg-gray-400 hover:bg-gray-500 text-white rounded-full p-2 px-4">
-                <h1 className="text-lg">เลือกห้องประชุม</h1>
+              <button
+                onClick={() => setTabSelect("room")}
+                className="w-fit bg-gray-400 hover:bg-gray-500 text-white rounded-full p-2 px-4"
+              >
+                <h1 className="">เลือกห้องประชุม</h1>
               </button>
-              <button className="w-fit bg-gray-400 hover:bg-gray-500 text-white rounded-full p-2 px-4">
-                <h1 className="text-lg">เลือกเวลา</h1>
+              <button
+                onClick={() => setTabSelect("time")}
+                className="w-fit bg-gray-400 hover:bg-gray-500 text-white rounded-full p-2 px-4"
+              >
+                <h1 className="">เลือกเวลา</h1>
               </button>
             </div>
-            {false && (
+            {tabSelect === "room" && (
               <div className="h-full border-2 rounded-lg p-4">
                 <div className="text-center mb-4">
                   <p>กรุณาเลือกห้องประชุมที่ต้องการ</p>
                 </div>
-                <div className="roomList h-[300px] flex  flex-col gap-y-4 overflow-y-auto">
+                <div className="roomList flex md:max-h-[400px] flex-col gap-y-4 overflow-y-auto">
                   <button className="w-full bg-orange-200 hover:bg-orange-300 rounded-full p-2 px-4">
-                    ห้องประชุม 1, 8 คน, ราคา 800 บาท/ชม.
+                    ห้องประชุม 1, 8 คน
                   </button>
                   <button className="w-full bg-orange-200 hover:bg-orange-300 rounded-full p-2 px-4">
-                    ห้องประชุม 1, 8 คน, ราคา 800 บาท/ชม.
+                    ห้องประชุม 1, 8 คน
                   </button>
                   <button className="w-full bg-orange-200 hover:bg-orange-300 rounded-full p-2 px-4">
-                    ห้องประชุม 1, 8 คน, ราคา 800 บาท/ชม.
+                    ห้องประชุม 1, 8 คน
                   </button>
                   <button className="w-full bg-orange-200 hover:bg-orange-300 rounded-full p-2 px-4">
-                    ห้องประชุม 1, 8 คน, ราคา 800 บาท/ชม.
+                    ห้องประชุม 1, 8 คน
                   </button>
                   <button className="w-full bg-orange-200 hover:bg-orange-300 rounded-full p-2 px-4">
-                    ห้องประชุม 1, 8 คน, ราคา 800 บาท/ชม.
+                    ห้องประชุม 1, 8 คน
                   </button>
                   <button className="w-full bg-orange-200 hover:bg-orange-300 rounded-full p-2 px-4">
-                    ห้องประชุม 1, 8 คน, ราคา 800 บาท/ชม.
+                    ห้องประชุม 1, 8 คน
                   </button>
                   <button className="w-full bg-orange-200 hover:bg-orange-300 rounded-full p-2 px-4">
-                    ห้องประชุม 1, 8 คน, ราคา 800 บาท/ชม.
+                    ห้องประชุม 1, 8 คน
                   </button>
                   <button className="w-full bg-orange-200 hover:bg-orange-300 rounded-full p-2 px-4">
-                    ห้องประชุม 1, 8 คน, ราคา 800 บาท/ชม.
+                    ห้องประชุม 1, 8 คน
                   </button>
                   <button className="w-full bg-orange-200 hover:bg-orange-300 rounded-full p-2 px-4">
-                    ห้องประชุม 1, 8 คน, ราคา 800 บาท/ชม.
+                    ห้องประชุม 1, 8 คน
                   </button>
                   <button className="w-full bg-orange-200 hover:bg-orange-300 rounded-full p-2 px-4">
-                    ห้องประชุม 1, 8 คน, ราคา 800 บาท/ชม.
+                    ห้องประชุม 1, 8 คน
                   </button>
                 </div>
               </div>
             )}
 
-            {true && (
+            {tabSelect === "time" && (
               <div className="h-full border-2 rounded-lg p-4">
                 <div className="text-center mb-4">
                   <p>กรุณาเลือกเวลาที่ต้องการ</p>
                 </div>
-                <div className="roomList h-[300px] flex  flex-col gap-y-4 overflow-y-auto">
+                <div className="roomList flex  md:max-h-[400px] flex-col gap-y-4 overflow-y-auto">
                   <button className="w-full bg-orange-200 hover:bg-orange-300 rounded-full p-2 px-4">
                     8:00 - 9:00
                   </button>
